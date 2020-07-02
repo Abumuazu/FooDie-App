a3412202cd9f11ed108aa8c87c98aa9c36ba4507
Abu <63474683+Abumuazu@users.noreply.github.com>
2020-07-02 09:10:20 -0700

    Updates

---
+++ precache-manifest.409c9bf6e803ea1fbad5dc01a1581dd9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1078f9230155385e3639534cefef1b3b",
+    "url": "/fooDie-app/index.html"
+  },
+  {
+    "revision": "d0a910b751b12810d2e1",
+    "url": "/fooDie-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ac01477074d58c4d91da",
+    "url": "/fooDie-app/static/css/main.37550ef4.chunk.css"
+  },
+  {
+    "revision": "d0a910b751b12810d2e1",
+    "url": "/fooDie-app/static/js/2.fae3ada0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fooDie-app/static/js/2.fae3ada0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac01477074d58c4d91da",
+    "url": "/fooDie-app/static/js/main.b74b451d.chunk.js"
+  },
+  {
+    "revision": "d2f0a767c4570f1def6c",
+    "url": "/fooDie-app/static/js/runtime-main.39b3f23f.js"
+  },
+  {
+    "revision": "c4f1d0575bf49c6cf731bf59fc258246",
+    "url": "/fooDie-app/static/media/bake.c4f1d057.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FooDie-App/precache-manifest.51c203a38a3132e69fe37db5f955dbfe.js"
+  "/fooDie-app/precache-manifest.409c9bf6e803ea1fbad5dc01a1581dd9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FooDie-App/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fooDie-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
