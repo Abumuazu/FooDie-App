0238fb1124fa39036d92cef6bb119e12dec81666
Abu <63474683+Abumuazu@users.noreply.github.com>
2020-07-02 08:45:49 -0700

    Updates

---
+++ precache-manifest.51c203a38a3132e69fe37db5f955dbfe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd40b077475202120b3c156b339cc03f",
+    "url": "/FooDie-App/index.html"
+  },
+  {
+    "revision": "1fd77febc5a31afdad46",
+    "url": "/FooDie-App/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9b9ab572f3dbd28b0e6d",
+    "url": "/FooDie-App/static/css/main.37550ef4.chunk.css"
+  },
+  {
+    "revision": "1fd77febc5a31afdad46",
+    "url": "/FooDie-App/static/js/2.0d5590ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FooDie-App/static/js/2.0d5590ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b9ab572f3dbd28b0e6d",
+    "url": "/FooDie-App/static/js/main.c28ec216.chunk.js"
+  },
+  {
+    "revision": "8cdbeafdcc666e237033",
+    "url": "/FooDie-App/static/js/runtime-main.3292c9a6.js"
+  },
+  {
+    "revision": "c4f1d0575bf49c6cf731bf59fc258246",
+    "url": "/FooDie-App/static/media/bake.c4f1d057.png"
+  }
+]);
